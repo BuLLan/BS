185e2a0d2076f48ddf037cf164033b1bca4967f0
Dennis Körte <dennis.koerte@mailbox.tu-dresden.de>
2016-03-16 19:24:21 +0100

    Initial commit

---
+++ android/src/blockslide/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package blockslide.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import blockslide.game.BlockslideMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BlockslideMain(), config);
+	}
+}
---
+++ desktop/src/blockslide/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package blockslide.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import blockslide.game.BlockslideMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlockslideMain(), config);
+	}
+}
